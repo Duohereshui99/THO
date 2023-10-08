483f54cff6bab64f3408e6db21b32b2702ef4ac0
Bo Jizheng <1643769621@qq.com>
2023-10-08 15:12:57 +0800

    testmessage

--- THO.f
+++ THO.f
@@ -82,10 +82,10 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
             psi(i,j)=FFR4(rr(j)/hcm,psi_1,n_diff)
             d2psi(i,j)=FFR4(rr(j)/hcm,d2psi_1,n_diff)
             end do 
-      end do
-      do i=1,n_int
-            vpot(i)=FFR4(rr(i)/hcm,vpot_1,n_diff)
-      end do
+            end do
+            do i=1,n_int
+                  vpot(i)=FFR4(rr(i)/hcm,vpot_1,n_diff)
+            end do
 ccccccc
 
 ccccccc           
